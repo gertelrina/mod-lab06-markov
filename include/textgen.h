2b90053f7132545140137946dddc2647afed5089
gertelrina <gertelrina@yandex.ru>
2023-04-24 17:58:28 +0000

    fix linter3

--- include/textgen.h
+++ include/textgen.h
@@ -3,20 +3,21 @@
 #define INCLUDE_TEXTGEN_H_
 #include "deque"
 #include "map"
-#include "vector"
 #include "string"
+#include "vector"
 
 class MarkovChain {
- private:
+private:
   typedef std::deque<std::string> prefix;
   std::map<prefix, std::vector<std::string>> statetab;
   std::size_t words_in_prefix;
 
   std::string ConvertFromDeque(const prefix &prx);
 
- public:
+public:
   MarkovChain() = delete;
-  explicit MarkovChain(const std::string &base_str, std::size_t _words_in_prefix = 2);
+  explicit MarkovChain(const std::string &base_str,
+                       std::size_t _words_in_prefix = 2);
   std::string GenerateText(const std::size_t result_size = 1000);
   ~MarkovChain() {}
 };
